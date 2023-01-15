cec8f7ac2642640fda07f443afb5c5727338eb5d
John Doe <johndoe@example.com>
2023-01-15 17:03:10 +0800

    fix: Let yarn pack ignore junk files

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import copy from 'rollup-plugin-copy'
+import del from 'rollup-plugin-delete'
 
 export default [
   {
@@ -24,9 +26,33 @@ export default [
         format: 'es'
       }
     ],
-    plugins: [resolve(), typescript()],
-    watch: {
-      include: 'src/**'
-    }
+    plugins: [
+      resolve(),
+      typescript(),
+      copy({
+        targets: [
+          {
+            src: [
+              'templates/arco-vue/*',
+              'templates/arco-vue/.*',
+              '!templates/arco-vue/node_modules',
+              '!templates/arco-vue/dist',
+              '!templates/arco-vue/dist-ssr',
+
+              // The 'rollup-plugin-copy' will not ignore files in subdirectories,
+              // use 'rollup-plugin-delete' to delete them later.
+              '!templates/arco-vue/.env',
+              '!templates/arco-vue/.DS_Store',
+              '!templates/arco-vue/yarn-error.log'
+            ],
+            dest: 'dist/templates/arco-vue'
+          }
+        ],
+        verbose: true
+      }),
+      del({
+        targets: ['**/.DS_Store', '**/yarn-error.log']
+      })
+    ]
   }
 ]
